37e73835609ad0301d2d4ed776b0cab37a987574
ashikfec01 <kaziashik999@gmail.com>
2022-02-15 16:39:22 +0600

    initial setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,32 @@
-import { TestBed } from '@angular/core/testing';
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AppTopbarComponent } from './app.topbar.component';
+import { AppFooterComponent } from './app.footer.component';
+import { AppSideBarComponent } from './app.sidebar.component';
+import { AppSideBarTabContentComponent } from './app.sidebartabcontent.component';
+import { AppMenuComponent } from './app.menu.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+        imports: [ RouterTestingModule ],
+        declarations: [ AppComponent,
+                AppTopbarComponent,
+                AppMenuComponent,
+                AppFooterComponent,
+                AppSideBarComponent,
+                AppSideBarTabContentComponent
+            ]
+    });
+    TestBed.compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ng-invis'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-invis');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-invis app is running!');
-  });
+  }));
 });
