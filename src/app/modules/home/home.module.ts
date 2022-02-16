f7fcb7a7fe6fe57e938346472818d92d2b9c7706
ashikfec01 <kaziashik999@gmail.com>
2022-02-16 18:56:02 +0600

    Home module setup

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { AboutComponent } from './components/about/about.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { OfferComponent } from './components/offer/offer.component';
+import { ProductComponent } from './components/product/product.component';
+import { ServiceComponent } from './components/service/service.component';
+import { TeamComponent } from './components/team/team.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    HeaderComponent,
+    BannerComponent,
+    ProductComponent,
+    FooterComponent,
+    TeamComponent,
+    AboutComponent,
+    ServiceComponent,
+    OfferComponent,
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SharedModule,
+    MenubarModule,
+
+  ]
+})
+export class HomeModule { }
